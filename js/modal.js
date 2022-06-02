d0b8adf6040d33755c53fb72b42cbb07e2cbf4bf
Francisco Thiago <75057408+Francisco-Thiago@users.noreply.github.com>
2022-06-02 06:36:53 -0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,14 @@
 const modal = document.querySelector('.modal');
-const closeModal = document.querySelector('.close')
-const item = document.querySelector('.item')
+const closeModal = document.querySelector('.close');
+const item = document.querySelectorAll('.item');
 
-console.log(closeModal)
-
-
-item.addEventListener('click', (el) =>{
-    modal.style.display="flex"
-    console.log(el.target.parentNode)
+[...item].map(itemSelected => {
+    itemSelected.addEventListener('click', (el) =>{
+        modal.style.display = "flex";
+        console.log(el.target.parentNode);
+    })
 })
 
 closeModal.addEventListener('click', () => {
-    modal.style.display="none"
+    modal.style.display = "none";
 })
